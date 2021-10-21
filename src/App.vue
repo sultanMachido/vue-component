70492f63a0cc2c67c5e31763be7fe8e3f9f0a7ee
Ope <opehardesina@gmail.com>
2021-10-21 13:04:32 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,94 @@
+<template>
+  <Header title="Task Tracker" :info="info" active="info-active" inactive="info-inactive"/>
+</template>
+
+<script>
+import Header from './components/Header.vue'
+
+export default {
+  name: 'App',
+  components:{
+    Header
+  },
+  data(){
+    return{
+      info:[
+        {
+        index:0,
+        text:'I am info one',
+        indicator:true
+      },
+        {
+        index:1,
+        text:'I am info two',
+        indicator:false
+      },
+        {
+        index:2,
+        text:'I am info three',
+        indicator:false
+      }
+      ],
+      currentIndicatorPosition:0,
+      // lastPosition: this.info.length - 1,
+      startPosition: 0,
+      val:'',
+      selected:[],
+      checkedNames:[],
+      message:[]
+    }
+  },
+  
+  methods:{
+    moveIndicator(){
+      let lastPosition = this.info.length - 1;
+      let nextPosition
+
+        if(this.currentIndicatorPosition === lastPosition){
+        console.log(this.info[this.startPosition],'here');
+         this.info[this.currentIndicatorPosition].indicator = false;
+        this.info[this.startPosition].indicator = true
+        this.currentIndicatorPosition = this.startPosition
+         console.log(this.info[this.startPosition],'here');       
+     }else{
+        this.info[this.currentIndicatorPosition].indicator = false;
+     nextPosition = this.currentIndicatorPosition + 1;
+     this.currentIndicatorPosition = nextPosition
+
+     console.log(this.currentIndicatorPosition,'current');
+     console.log(nextPosition,'next')
+
+    
+          this.info[nextPosition].indicator = true;
+   
+     }
+   
+    
+     
+     
+    
+
+    }
+  },
+  created(){
+    setInterval(this.moveIndicator,1000);
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.info-active{
+  border-left: 1px solid blue;
+}
+.info-inactive{
+  color:gray
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,32 @@
+<template>
+   <h1>{{title}}</h1> 
+   <div :key="inf.index" v-for="inf in info" :class="{active:inf.indicator,inactive:!inf.indicator}" class="info">
+       {{inf.text}}
+   </div>
+</template>
+
+
+<script>
+export default {
+    name:'Header',
+    props:{
+        title:String,
+        info: Array,
+        active:String,
+        inactive:String
+    }
+}
+</script>
+
+<style>
+ .active{
+  border-left: 1px solid blue;
+}
+.inactive{
+  color:gray
+}
+.info{
+    height:30px;
+    width:100px
+}
+</style>
